a148a8a6869b9b38606db997f3caa1169f98095c
ben lamothe <bl@732.one>
2019-10-18 03:24:28 +0200

    add app skeleton with login form

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext } from 'react';
+import 'react-bulma-components/dist/react-bulma-components.min.css';
+import './App.scss';
+import { Navbar, Container, Loader, Columns, Section } from 'react-bulma-components'
+import { LoginForm } from './pages/login';
+import { StoreProvider, StoreContext } from './state/store';
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import { UserMessageList } from './components/messagelist'
+
+const Routing = () => {
+  const [globalState] = useContext(StoreContext)
+
+  return (
+    globalState.loading > 0 ?
+      <Section>
+        <Columns className="is-desktop">
+          <Loader style={{ height: 100, width: 100 }} />
+        </Columns>
+      </Section>
+      :
+      <div>
+        <UserMessageList />
+        <Router>
+          <Switch>
+            <Route path="/login">
+              <LoginForm />
+            </Route>
+            <Route>
+              <LoginForm />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+  )
+}
+
+const NavBar = () => {
+  const [globalState] = useContext(StoreContext)
+
+  return (
+    <Navbar transparent={false}>
+      <Navbar.Brand>
+        <img src={require("./logo.svg")} alt="Tupelo" />
+      </Navbar.Brand>
+      <Navbar.Container position="end">
+        {globalState && globalState.username && <p>Decentratweets for {globalState.username}</p>}
+      </Navbar.Container>
+    </Navbar>
+  )
+}
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StoreProvider >
+      <Container>
+        <NavBar />
+      </Container>
+      <Container>
+        <Section>
+          <Routing />
+        </Section>
+      </Container>
+    </StoreProvider>
   );
 }
 
